493776e00adf70d47ed6c30bdec43329810d05b2
Kevin Christianson <christiansonk@carleton.edu>
2018-01-29 20:33:40 -0600

    Captin Prescot usage and runtime improvements

--- Assets/Scripts/CrowdSpawner.cs
+++ Assets/Scripts/CrowdSpawner.cs
@@ -1,16 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-//using System.Linq;
 using UnityEngine;
 
 public class CrowdSpawner : MonoBehaviour {
 
-	/*[SerializeField] private GameObject townsPeople;
+	[SerializeField] private GameObject townsPeople;
 	[SerializeField] private int spacing = 1;
     [SerializeField] private int sizeX = 15;
 	[SerializeField] private int sizeZ = 15;
 	[SerializeField] private int numPeople = 20;
-*/
+
 	// Use this for initialization
 	void Start () {
 		TownpersonSpawn ();
@@ -23,8 +22,6 @@ public class CrowdSpawner : MonoBehaviour {
 		int numMade = 0;
 		Vector3 location = GetComponent<Transform>().position;
 		while (numMade < numPeople) {
-//			int randX = rnd.Next (-spacing, spacing);
-//			int randZ = rnd.Next (-spacing, spacing);
 			int randX = Random.Range(-spacing, spacing);
 			int randZ = Random.Range(-spacing, spacing);
 			GameObject.Instantiate (townsPeople, new Vector3 (location.x + (numMade / colNum) + randX, location.y, location.z + (numMade / rowNum) + randZ), Quaternion.identity);
