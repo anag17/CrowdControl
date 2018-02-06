2b547d5a4ee70d9aa324831cea600dcdb6ab54cc
Samuel Nozaki <samuel.nozaki@gmail.com>
2018-02-05 20:26:45 -0600

    Made a functional weighted random crowd

--- Assets/Scripts/CrowdSpawner.cs
+++ Assets/Scripts/CrowdSpawner.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class CrowdSpawner : MonoBehaviour {
 
 	[SerializeField] private GameObject townsPeople;
-	[SerializeField] private int spacing = 1;
     [SerializeField] private int sizeX = 15;
 	[SerializeField] private int sizeZ = 15;
 	[SerializeField] private int numPeople = 20;
+    [SerializeField] private float spacing = 1.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -16,16 +16,45 @@ public class CrowdSpawner : MonoBehaviour {
 	}
 
 	private void TownpersonSpawn () {
-        //Random rnd = new Random();
-        int colNum = sizeX / (2 * spacing + 1);
-		int rowNum = sizeZ / (2 * spacing + 1);
-		int numMade = 0;
-		Vector3 location = GetComponent<Transform>().position;
+        Vector3 location = GetComponent<Transform>().position;
+        bool[,] openLocation = new bool[sizeX, sizeZ];
+        int[] focalPoint = { 0, sizeZ / 2 };
+        int numMade = 0;
+
+        for (int i = 0; i < sizeX; i++) {
+            for (int j = 0; j < sizeZ; j++) {
+                openLocation[i, j] = true;
+            }
+        }
+
+		
 		while (numMade < numPeople) {
-			int randX = Random.Range(-spacing, spacing);
-			int randZ = Random.Range(-spacing, spacing);
-			GameObject.Instantiate (townsPeople, new Vector3 (location.x + (numMade % colNum) + randX, location.y, location.z + (numMade / rowNum) + randZ), Quaternion.identity);
-			numMade++;
+            for (int i = 0; i < sizeX; i++) {
+                for (int j = 0; j < sizeZ; j++) {
+                    if (openLocation[i, j]) {
+                        float distance = ((i - focalPoint[0]) * (i - focalPoint[0]) + (j - focalPoint[1]) * (j - focalPoint[1]));
+                        if (distance < 1) {
+                            distance = 1;
+                        }
+
+                        float chance = (1.0f / distance);
+                        if (Random.Range(0.0f, 1.0f) <= chance) {
+                            float randX = Random.Range(-spacing / 2, spacing / 2);
+                            float randZ = Random.Range(-spacing / 2, spacing / 2);
+                            GameObject.Instantiate(townsPeople, new Vector3(location.x + spacing * (i + randX), location.y, location.z + spacing * (j + randZ)), Quaternion.identity);
+                            openLocation[i, j] = false;
+                            numMade++;
+                        }
+
+                        if (numMade == numPeople) { break; }
+                    }
+
+                    if (numMade == numPeople) { break; }
+                }
+            }
+
+            
+			
 		}
 	}
 }
