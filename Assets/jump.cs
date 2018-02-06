c1e20f9d6b8e65a5cf7413c10e962ede069f3246
Kevin Christianson <christiansonk@carleton.edu>
2018-02-05 21:28:46 -0600

    fixed merge problems

--- Assets/jump.cs
+++ Assets/jump.cs
@@ -6,34 +6,29 @@ public class jump : MonoBehaviour {
 
 	public enum MOOD { Calm = 0, Agitated = 1, Hostile = 2 };
 	[SerializeField] private MOOD crowdMood;
-	[SerializeField] private float jumpHeight;
+	[SerializeField] private int jumpHeight;
 	[SerializeField] private int jumpProbability;
 	private bool jumping = false;
-    private Transform trans;
+	private Transform trans;
 
-    private void Start() {
-        trans = this.GetComponent<Transform>();
-    }
+	private void Start() {
+		trans = this.GetComponent<Transform>();
+	}
 
-    // Update is called once per frame
-    void Update () {
-        trans.rotation = new Quaternion(0, 0, 0, 0);
+	// Update is called once per frame
+	void Update () {
+		trans.rotation = new Quaternion(0, 0, 0, 0);
 		if (!jumping) {
 			int random = Random.Range(0, 100 - jumpProbability);
 			if (random == 0) {
+				gameObject.GetComponent<Rigidbody>().AddForce(Vector3.up * (jumpHeight * (int)crowdMood));
 				jumping = true;
 			}
-		} else {
-			if (!falling && position.y < jumpHeight) {
-				gameObject.GetComponent<Rigidbody>().MovePosition(new Vector3(position.x, position.y + 0.1f, position.z));
-			} else if (!falling && position.y >= jumpHeight) {
-				falling = true;
-			} else if (falling && position.y <= 1) {
-				falling = false;
-				jumping = false;
-			} else {
-				gameObject.GetComponent<Rigidbody>().MovePosition(new Vector3(position.x, position.y - 0.1f, position.z));
-			}
 		}
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		jumping = false;
 	}
 }
