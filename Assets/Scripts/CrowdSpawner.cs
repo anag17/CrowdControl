d4f499a66e5f5ad4e59545f4e0c5f494340175cf
Kevin Christianson <christiansonk@carleton.edu>
2018-02-05 20:37:33 -0600

    basic distribution of different types of townspeople

--- Assets/Scripts/CrowdSpawner.cs
+++ Assets/Scripts/CrowdSpawner.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CrowdSpawner : MonoBehaviour {
 
 	[SerializeField] private GameObject townsPeople;
+	[SerializeField] private Material[] materials;
     [SerializeField] private int sizeX = 15;
 	[SerializeField] private int sizeZ = 15;
 	[SerializeField] private int numPeople = 20;
@@ -41,7 +42,7 @@ public class CrowdSpawner : MonoBehaviour {
                         if (Random.Range(0.0f, 1.0f) <= chance) {
                             float randX = Random.Range(-spacing / 2, spacing / 2);
                             float randZ = Random.Range(-spacing / 2, spacing / 2);
-                            GameObject.Instantiate(townsPeople, new Vector3(location.x + spacing * (i + randX), location.y, location.z + spacing * (j + randZ)), Quaternion.identity);
+							GameObject.Instantiate(townsPeople, new Vector3(location.x + spacing * (i + randX), location.y, location.z + spacing * (j + randZ)), Quaternion.identity).GetComponent<Renderer>().material = materials[numMade % materials.Length];;
                             openLocation[i, j] = false;
                             numMade++;
                         }
