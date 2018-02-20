e548fbf2512ff1540f907a264ccfc61ea64b15aa
martinchoffman <martin.c.hoffman@gmail.com>
2018-02-19 22:03:28 -0600

    Stuff

---
+++ Assets/Scripts/updateFog.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class updateFog : MonoBehaviour {
+    public float startDistance = 10;
+    public float endDistance = 100;
+    public GameObject otherCamera;
+
+	// Use this for initialization
+	void Start () {
+        OnEnable();
+	}
+
+    private void OnEnable() {
+        RenderSettings.fogMode = FogMode.Linear;
+        RenderSettings.fogStartDistance = startDistance;
+        RenderSettings.fogEndDistance = endDistance;
+    }
+    
+    void Update () {
+        // Switch camera for testing purposes
+        if (Input.GetKeyDown("c")) {
+            otherCamera.SetActive(true);
+            this.gameObject.SetActive(false);
+        }
+	}
+
+    public void PlayAnimation() {
+        GetComponent<Animator>().SetBool("ChoicesDone", true);
+    }
+}
