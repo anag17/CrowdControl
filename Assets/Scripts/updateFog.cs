1033158f8ba23466178d79fb9e25afdf833fb0f8
Martin <hoffmanm2@carleton.edu>
2018-02-19 21:55:45 -0600

    Added sexy moonlight and more fog options

---
+++ Assets/Scripts/updateFog.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class updateFog : MonoBehaviour {
+    [SerializeField] private bool isFoggy = false;
+    [SerializeField] private float startDistance = 10;
+    [SerializeField] private float endDistance = 100;
+    [SerializeField] private Color fogColor = new Color(1, 1, 1, 1);
+
+	// Use this for initialization
+	void Start () {
+        OnEnable();
+	}
+
+    private void OnEnable() {
+        RenderSettings.fog = isFoggy;
+        RenderSettings.fogMode = FogMode.Linear;
+        RenderSettings.fogStartDistance = startDistance;
+        RenderSettings.fogEndDistance = endDistance;
+        RenderSettings.fogColor = fogColor;
+    }
+}
